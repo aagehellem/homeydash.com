228ec6dbb041a0f94abc8d9a3b6bd6e284d0efd3
aagehellem <aage@hellem.net>
2025-06-03 09:46:07 +0200

    Update homeydash.app.js

--- app/js/homeydash.app.js
+++ app/js/homeydash.app.js
@@ -360,45 +360,43 @@ window.addEventListener('load', function() {
         return renderAlarms(alarms);
       }).catch(console.error);
 
-homey.devices.getDevices().then(function(devices) {
-  var favoriteDevices = me.properties.favoriteDevices.map(function(deviceId){
-    return devices[deviceId];
-  }).filter(function(device){
-    return !!device;
-  }).filter(function(device){
-    if(!device.ui) return false;
-    return true;
-  });
-
-  // Weather icon from YR device
-  for (const id in devices) {
-    const device = devices[id];
-    console.log("Device found:", device.name, "-", device.driverUri);
-
-    if (device.driverUri === "homey:app:no.yr:myr" && device.ready) {
-      console.log("Found YR device");
-
-      if (device.capabilitiesObj.weather_description) {
-        const condition = device.capabilitiesObj.weather_description.value
-          .toLowerCase()
-          .replace(/\s+/g, '');
-
-        console.log("YR condition:", condition);
-
-        $weatherStateIcon.classList.add(condition);
-        $weatherStateIcon.style.backgroundImage = `url(img/weather/${condition}.svg)`;
-        $weatherStateIcon.style.webkitMaskImage = `url(img/weather/${condition}.svg)`;
-      } else {
-        console.warn("YR device has no weather_description capability at this time.");
-      }
-
-      break;
-    }
-  }
-
-});
-
-        
+      homey.devices.getDevices().then(function(devices) {
+        var favoriteDevices = me.properties.favoriteDevices.map(function(deviceId){
+          return devices[deviceId];
+        }).filter(function(device){
+          return !!device;
+        }).filter(function(device){
+          if(!device.ui) return false;
+          return true;
+        });
+      
+        // Weather icon from YR device
+        for (const id in devices) {
+          const device = devices[id];
+          console.log("Device found:", device.name, "-", device.driverUri);
+      
+          if (device.driverUri === "homey:app:no.yr:myr" && device.ready) {
+            console.log("Found YR device");
+      
+            if (device.capabilitiesObj.weather_description) {
+              const condition = device.capabilitiesObj.weather_description.value
+                .toLowerCase()
+                .replace(/\s+/g, '');
+      
+              console.log("YR condition:", condition);
+      
+              $weatherStateIcon.classList.add(condition);
+              $weatherStateIcon.style.backgroundImage = `url(img/weather/${condition}.svg)`;
+              $weatherStateIcon.style.webkitMaskImage = `url(img/weather/${condition}.svg)`;
+            } else {
+              console.warn("YR device has no weather_description capability at this time.");
+            }
+      
+            break;
+          }
+        }
+      
+         
         favoriteDevices.forEach(function(device){
           // console.log(device.name)
           // console.log(device.capabilitiesObj)
