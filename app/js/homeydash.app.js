452dbec767859989c4b5cce31c831a8b82445bea
aagehellem <aage@hellem.net>
2025-06-06 19:28:49 +0200

    Update homeydash.app.js

--- app/js/homeydash.app.js
+++ app/js/homeydash.app.js
@@ -828,7 +828,8 @@ setTimeout(() => {
     // Set size and layout
     icon.style.width = '90px';
     icon.style.height = '90px';
-    icon.style.margin = 'auto';
+    icon.style.marginLeft = '0'
+    icon.style.marginRight = 'auto';
     icon.style.display = 'block';
 
     
